a1c71f8db11541c3d7f487a64cdd21acfd87735e
Zenker, Dr. Klaus (FWKE) - 126506 <k.zenker@hzdr.de>
2020-12-14 18:39:44 +0100

    Separate dummy class. Add remote get/set methods to the server.

---
+++ test/DummyServer/DummyServer.h
@@ -0,0 +1,95 @@
+/*
+ * DummyServer.h
+ *
+ *  Created on: Dec 11, 2020
+ *      Author: Klaus Zenker (HZDR)
+ */
+
+#pragma once
+
+#include "open62541.h"
+
+#include <thread>
+
+#include <boost/fusion/container/map.hpp>
+
+namespace fusion = boost::fusion;
+
+typedef fusion::map<
+    fusion::pair<UA_Int16, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_UInt16, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_Int32, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_UInt32, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_Int64, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_UInt64, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_Double, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_Float, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_String, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_SByte, std::pair<std::string,UA_DataType> >
+  , fusion::pair<UA_Byte, std::pair<std::string,UA_DataType> > > TypeMapWithName;
+
+extern TypeMapWithName dummyMap;
+
+struct OPCUAServer{
+
+  OPCUAServer(): _config(UA_ServerConfig_standard), _server(nullptr){
+  }
+
+  ~OPCUAServer();
+
+  UA_ServerConfig _config;
+  UA_Server *_server;
+  UA_ServerNetworkLayer _nl;
+
+  UA_Boolean running = true;
+
+  void _start(uint port);
+
+  void start();
+
+  /*
+   * Stop the server by setting running to false
+   * This can be done by a signal handler or by setting running to false from outside -
+   * so no interaction with the server is needed...
+   */
+  void stop(){
+    running = false;
+  }
+
+  uint drawPort();
+
+  void addFolder(std::string name, UA_NodeId parent);
+
+  void addVariables();
+
+  template <typename UAType>
+  void setValue(std::string nodeName, const UAType &t);
+
+  UA_Variant* getValue(std::string nodeName);
+
+};
+
+template <typename UAType>
+void OPCUAServer::setValue(std::string nodeName, const UAType &t){
+  UA_Variant* data = UA_Variant_new();
+  UA_Variant_setScalarCopy(data,&t,&fusion::at_key<UAType>(dummyMap).second);
+  UA_Server_writeValue(_server, UA_NODEID_STRING(1, &nodeName[0]), *data);
+  UA_Variant_delete(data);
+}
+
+class ThreadedOPCUAServer {
+private:
+  std::thread _serverThread;
+
+public:
+  ThreadedOPCUAServer(): _serverThread(&OPCUAServer::start, &_server){
+
+  }
+
+  ~ThreadedOPCUAServer(){
+    _server.stop();
+    _serverThread.join();
+  }
+
+  OPCUAServer _server;
+};
